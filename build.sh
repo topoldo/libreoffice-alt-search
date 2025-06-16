1c28ebe11aa76ee7e70cd95d9ea04bee9ddf0642
gitxpy <ff.py@proton.me>
2025-06-16 13:24:07 +0200

    bashify a bit ;)

--- build.sh
+++ build.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
  
-# This file update the information of versions in description.xml
+# This script updates the information of version in description.xml
 # and  the update information.xml
 # and build (compress) the extension
 
 Ext="AltSearch"
-RotExt="Code"
+RootExt="Code"
+
+# I use 7zip, but you can use zip:
+ZIP_CMD='zip -ru ' 
+#ZIP_CMD='7z -tzip a '
 
 if [[ ! $1 ]]
 then
-	echo "Usage:"
-	echo "./build.sh [version]"
-	echo "example: ./build.sh 12.2.3"
-	exit
-	else
+	cat <<__USAGE
+Usage:
+$0 version
+example: ./build.sh 12.2.3beta4
+__USAGE
+
+else
 	echo
-    echo "### updating " $RotExt"/description.xml"
-   cd $RotExt
+    echo "### updating $RootExt/description.xml"
+    cd $RootExt
+
 	sed -i s'/<version value.*/<version value="'$1'"\/>/' description.xml
+
     echo "### Compiling " $Ext
-    # I use 7zip but you can use zip:
-    # zip -r $Ext.oxt .
-     7z a ./$Ext.oxt . -tzip
+    ${ZIP_CMD} ../$Ext.oxt .
+
      cd ..
-    mv $RotExt/$Ext.oxt ./
+ 
     echo 
-    echo "### updating " $Ext".update.xml"
+    echo "### updating $Ext.update.xml"
+
 	sed -i s'/<version value.*/<version value="'$1'"\/>/' $Ext.update.xml
 #only if oxt is in the release 	sed -i s'/raw.*/raw\/'$1'\/'$Ext'.oxt"\/>/' $Ext.update.xml
     echo "### Done"
