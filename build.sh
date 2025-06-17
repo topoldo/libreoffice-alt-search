2ee86b78d4af49bde5aafc3efc50f9d38cb44134
fpy <ff.py@proton.me>
2025-06-17 13:45:00 +0200

    bashify a bit more

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ RootExt=Code
 ZIP_CMD='7z -tzip a '
 
 SubstVersion() {
+	echo
+	echo "### updating  $2"
 	sed -i 's#<version value.*#<version value="'$1'"/>#' $2
 }
 
@@ -24,19 +26,14 @@ example: ./build.sh 12.2.3beta4
 __USAGE
 
 else
-	echo
-    echo "### updating $RootExt/description.xml"
-    cd $RootExt
-	SubstVersion $1 description.xml
+	SubstVersion $1 $RootExt/description.xml
 
+    cd $RootExt
     echo "### Compiling " $Ext
     ${ZIP_CMD} ../$Ext.oxt .
 
-     cd ..
+    cd ..
  
-    echo 
-    echo "### updating $Ext.update.xml"
-
 	SubstVersion $1 $Ext.update.xml
 
 #only if oxt is in the release 	sed -i s'/raw.*/raw\/'$1'\/'$Ext'.oxt"\/>/' $Ext.update.xml
